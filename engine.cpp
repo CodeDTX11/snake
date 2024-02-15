07a46bb61309c4af2f128a633f70574f30731067
Dylan <dm5messerly@gmail.com>
2024-02-15 14:24:42 -0600

    Working copy for 2d grid and 2 pointer snake logic

--- engine.cpp
+++ engine.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <conio.h> //console input/output
 #include <time.h>
-#include <deque>
+// #include <deque>
 
 #include "engine.h"
 
@@ -16,6 +16,7 @@ const int displayWidth = WIDTH;
 const int displayHeight = HEIGHT;
 
 pair<int,int> head;
+pair<int,int> tail;
 int curScore;
 
 char mode;
@@ -24,35 +25,40 @@ char hd = '0'; //snake head display
 char bd = 'o'; //snake body display
 char ms = '@'; //mouse display
 
-// deque<pair<int,int>> snake;
-
-enum direction { STOP = 0, LEFT, RIGHT, UP, DOWN} dir;
+enum direction {LEFT, RIGHT, UP, DOWN, STOP} dir;
 // direction dir; //direction the snake moves in
 
-char grid[HEIGHT][WIDTH] = {};
+pair<int,int> moveMnt[] = {{-1,0}, {1,0}, {0,-1}, {0,1}, {0,0}}; // used for snake movement
+
+pair<char, direction> grid[HEIGHT][WIDTH] = {}; // game board grid
+
+pair<int,int> operator +=(pair<int,int> &a, pair<int,int> &b) { // overload operator for pair
+    // return {a.first + b.first, a.second + b.second};
+    a.first = a.first + b.first;
+    a.second = a.second + b.second;
+}
+pair<int,int> operator +(pair<int,int> &a, pair<int,int> &b) { // overload op for pair
+    return {a.first + b.first, a.second + b.second};
+}
 
 void setup(){ //set up initial snake and mouse location on gamed display
     gameOver = false;
     quit = false;
     pause = false;
-    // snake.clear();
     srand(time(0));
-    // snake.push_front({displayWidth/2, displayHeight/2}); // initiate head of snake
 
-    for(int row = 0; row < HEIGHT; row++){
+    for(int row = 0; row < HEIGHT; row++){ // initialize grid with spaces
         for(int col = 0; col < WIDTH; col++){
-            grid[row][col] = ' ';
+            grid[row][col].first = ' ';
         }
     }
 
     head = {displayWidth/2, displayHeight/2}; // retain snake head for processing
+    tail = {head.first, head.second}; // start out tail pointer/coordinates same as head
 
-    grid[head.second][head.first] = hd;// seed snake head
+    grid[head.second][head.first].first = hd;// seed snake head
 
-    // mouseX = rand() % displayWidth;
-    // mouseY = rand() % displayHeight;
-
-    grid[rand() % displayHeight][rand() % displayWidth] = ms; // seed mouse
+    grid[rand() % displayHeight][rand() % displayWidth].first = ms; // seed mouse
 
     curScore = 0;
     dir = STOP;
@@ -70,7 +76,7 @@ void grid_prnt(){
     for(int row = 0; row < HEIGHT; row++){
         cout << '|';
         for(int col = 0; col < WIDTH; col++){
-            cout << grid[row][col];
+            cout << grid[row][col].first;
         }
         cout << '|' << endl;
     }
@@ -79,52 +85,6 @@ void grid_prnt(){
     }
     cout << endl;
 }
-
-// void draw(){ // print 2d game display
-   
-//     system("cls"); // clears ouput of previous draw
-//     cout << endl;
-    
-//     for(int i = 0; i < displayWidth + 2; i++){
-//         cout << "=";
-//     }
-//     cout << endl;
-//     int snakePrint = 0; // reset for each draw
-//     for (int row = 0; row < displayHeight; row++) {
-//         cout << "|";
-
-//         for (int col = 0; col < displayWidth; col++) {
-
-//             if (row == mouseY && col == mouseX){
-//                 cout << "@";
-//             } else if (snakePrint < snake.size()) { // checks if tail needs to be printed
-                
-//                 int i = 0;
-//                 for ( ; i < snake.size(); i++) { // iterate through snake to see if needs printing
-//                     if (snake[i].first == col && snake[i].second == row) { 
-//                         if(i == 0){
-//                             cout << "0";
-//                         } else {
-//                             cout << "o";
-//                         }
-//                         ++snakePrint;
-//                         break; // break out of loop once snake found for particular grid location
-//                     } 
-//                 }
-//                 if (i == snake.size()){ // if tail not printed, print space for grid
-//                     cout << " ";
-//                 }
-//             } else { 
-//                 cout << " ";
-//             }
-//         }
-//         cout << "|" << endl;
-//     }
-//     for(int i = 0; i < displayWidth + 2; i++){
-//         cout << "=";
-//     }
-//     cout << endl;
-// }
 
 void input() {
 
@@ -162,87 +122,87 @@ void input() {
 }
 
 void logic () {
+    if(dir != STOP){
+        // pair<int,int> tail = {head.first, head.second}; 
 
-    pair<int,int> tail = {head.first, head.second}; 
-    
-    switch (dir) { //snake head advances under current direction
-        case LEFT:
-            --head.first;
-            break;
-        case RIGHT:
-            ++head.first;
-            break;
-        case UP:
-            --head.second;
-            break;
-        case DOWN:
-            ++head.second;
-            break;
-        default:
-            break;
-    }
-
-    bool snakeExtend = false;
-    if (grid[head.second][head.first] == ms) { //logic when snake eats the mouse
-        // srand(time(0)); // Random seed value for rand based on time
+        grid[head.second][head.first] = {bd, dir};
         
-        // grid[head.second][head.first] = hd; // replace ? TODO
+        switch (dir) { //snake head advances under current direction
+            case LEFT:
+                --head.first;
+                break;
+            case RIGHT:
+                ++head.first;
+                break;
+            case UP:
+                --head.second;
+                break;
+            case DOWN:
+                ++head.second;
+                break;
+            default:
+                break;
+        }
+
+        bool snakeExtend = false;
+        if (grid[head.second][head.first].first == ms) { //logic when snake eats the mouse
+            
+            ++curScore;
+
+            grid[rand() % displayHeight][rand() % displayWidth].first = ms; // mouse
+
+            snakeExtend = true; // if mouse is eaten then the snake will grow/extend
+        }
+
+        if(mode == '1'){
+            //check if snake head hits out of bounds (harder)
+            if(head.first < 0 || head.first == displayWidth  || head.second < 0 || head.second == displayHeight ){ 
+                gameOver = true;
+                grid[head.second][head.first].first = 'X';
+                cout << "\nOut of bounds" << endl;
+            }
+        } else {
+            //alternatively the code below wraps the snake around borders instead of hitting out of bounds (easier)
+            if(head.first < 0) {
+                head.first = displayWidth - 1;
+            } else if (head.first == displayWidth ){
+                // gameOver = true;
+                head.first = 0;
+            } else if (head.second < 0){
+                head.second = displayHeight - 1;
+            } else if (head.second >= displayHeight){ 
+                head.second = 0;
+            }
+        }
         
-        ++curScore;
+        if(snakeExtend == false){  // advance tail pointer/coordinates if extend did not occur
+            direction tailDirection = grid[tail.second][tail.first].second;
+            
+            grid[tail.second][tail.first].first = ' ';
+            grid[tail.second][tail.first].second = {STOP};
 
-        grid[rand() % displayHeight][rand() % displayWidth] = ms; // mouse
+            // tail = tail + moveMnt[tailDirection];
+            tail += moveMnt[tailDirection];
 
-        // mouseX = rand() % displayWidth;
-        // mouseY = rand() % displayHeight;
-        
-        snakeExtend = true; // if mouse is eaten then the snake will grow/extend
-    }
-    
-    // pair<int,int> snakeHead = snake.front(); // create new snakehead variable
+            if(mode == '2'){ // check for snake tail wrapping around while following head
+                if(tail.first < 0) {
+                    tail.first = displayWidth - 1;
+                } else if (tail.first == displayWidth ){
+                    tail.first = 0;
+                } else if (tail.second < 0){
+                    tail.second = displayHeight - 1;
+                } else if (tail.second >= displayHeight){ 
+                    tail.second = 0;
+                }
+            }
+        } // if extend did occur then tail pointer remains the same
 
-
-    // if(snake.size() > 1 && snake[1] == snakeHead){ // check for snake eating self
-    //     gameOver = true;
-    //     cout << "\nCollison occured" << endl;
-    // }
-
-    // snake.push_front(snakeHead); //deque for snake, push current head position onto dq
-
-    // if(snakeExtend == false){
-    //     snake.pop_back(); //pop back if snake not extended to simulate snake advancing
-    //     // if snake does extend, do not pop back since that is where the snake grows
-    // } 
-
-    if(mode == '1'){
-        //check if snake head hits out of bounds (harder)
-        if(head.first < 0 || head.first == displayWidth  || head.second < 0 || head.second == displayHeight ){ 
+        if (grid[head.second][head.first].first == bd){ // check for snake eating self
             gameOver = true;
-            grid[head.second][head.first] = 'X';
-            cout << "\nOut of bounds" << endl;
+            grid[head.second][head.first].first = 'X';
+            cout << "\nYou bit yourself" << endl;
         }
-    } else {
-        //alternatively the code below wraps the snake around borders instead of hitting out of bounds (easier)
-        if(head.first < 0){
-            head.first = displayWidth - 1;
-        } else if (head.first == displayWidth ){
-            // gameOver = true;
-            head.first = 0;
-        } else if (head.second < 0){
-            head.second = displayHeight - 1;
-        } else if (head.second >= displayHeight){ 
-            head.second = 0;
-        }
-    }
 
-    if (grid[head.second][head.first] == bd){ // check for snake eating self TODO
-        gameOver = true;
-        grid[head.second][head.first] = 'X';
-        cout << "\nYou bit yourself" << endl;
+        grid[head.second][head.first].first = hd; // reasign head display value
     }
-    
-    
-    if(snakeExtend == false){
-        grid[tail.second][tail.first] = ' ';
-    }
-    grid[head.second][head.first] = hd;
 }
